8a68000606c21fbe452732c02a01b950155618c0
MarkMwago2 <markmwago@gmail.com>
2019-10-21 11:44:53 +0300

    make the main activity functional

--- app/src/main/java/com/example/eventapplication/MainActivity.java
+++ app/src/main/java/com/example/eventapplication/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.eventapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button mAddEventButton;
+    private Button mViewEventButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAddEventButton = (Button) findViewById(R.id.AddEvent);
+        mViewEventButton = (Button) findViewById(R.id.ViewEvent);
+
+
+        mAddEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, AddEventsActivity.class);
+                startActivity(intent1);
+            }
+        });
+        mViewEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, ViewEventsActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
