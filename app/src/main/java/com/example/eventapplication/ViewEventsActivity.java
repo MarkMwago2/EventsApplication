f90b27eebda289f3d8c8b36f063eec5ced1341c5
MarkMwago2 <markmwago@gmail.com>
2019-10-23 04:36:16 +0300

    pass events created to the View Events Activity

--- app/src/main/java/com/example/eventapplication/ViewEventsActivity.java
+++ app/src/main/java/com/example/eventapplication/ViewEventsActivity.java
@@ -2,12 +2,24 @@ package com.example.eventapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewEventsActivity extends AppCompatActivity {
+    TextView events;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_events);
+        events = (TextView) findViewById(R.id.EventText);
+
+        events.setText(getIntent().getStringExtra("Title"));
+        events.setText(getIntent().getStringExtra("StartDate"));
+        events.setText(getIntent().getStringExtra("EndDate"));
+        events.setText(getIntent().getStringExtra("StartTime"));
+        events.setText(getIntent().getStringExtra("EndTime"));
+        events.setText(getIntent().getStringExtra("Description"));
+
+
     }
 }
